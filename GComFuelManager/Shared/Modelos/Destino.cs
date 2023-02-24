56e2596bb1095929368f5539ae0f00091fbcd5c7
AngelZapata582 <angelzapata582@gmail.com>
2023-02-24 17:54:17 -0600

    "agregado modelos"

---
+++ GComFuelManager/Shared/Modelos/Destino.cs
@@ -0,0 +1,46 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GComFuelManager.Shared.Modelos
+{
+    public class Destino
+    {
+        [JsonProperty("cod")]
+        public int Cod { get; set; }
+        [JsonProperty("num")]
+        public int? Num { get; set; } = 0;
+        [JsonProperty("den"),MaxLength(128)]
+        public string? Den { get; set; } = string.Empty;
+        [JsonProperty("codcte")]
+        public int? Codcte { get; set; } = 0;
+        [JsonProperty("nroper"), MaxLength(30)]
+        public string? Nroper { get; set; } = string.Empty;
+        [JsonProperty("dir")]
+        public string? Dir { get; set; } = string.Empty;
+        [JsonProperty("codbdTan")]
+        public int? CodbdTan { get; set; } = 0;
+        [JsonProperty("descod"), MaxLength(10)]
+        public string? DesCod { get; set; } = string.Empty;
+        [JsonProperty("codsyn"), MaxLength(20)]
+        public string? Codsyn { get; set; } = string.Empty;
+        [JsonProperty("esenergas")]
+        public bool? Esenergas { get; set; } = false;
+        [JsonProperty("activo")]
+        public bool? Activo { get; set; } = true;
+        [JsonProperty("lat"), MaxLength(50)]
+        public string? Lat { get; set; } = string.Empty;
+        [JsonProperty("lon"), MaxLength(50)]
+        public string? Lon { get; set; } = string.Empty;
+        [JsonProperty("codciu")]
+        public int? Codciu { get; set; } = 0;
+        [JsonProperty("ciu"), MaxLength(50)]
+        public string? Ciu { get; set; } = string.Empty;
+        [JsonProperty("est"), MaxLength(50)]
+        public string? Est { get; set; } = string.Empty;
+    }
+}
--- GComFuelManager/Shared/Modelos/Usuario.cs
+++ GComFuelManager/Shared/Modelos/Usuario.cs
@@ -1,6 +1,7 @@
 ﻿using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Data.Common;
 using System.Linq;
 using System.Text;
@@ -11,22 +12,22 @@ namespace GComFuelManager.Shared.Modelos
     public class Usuario
     {
         [JsonProperty("cod")]
-        public int Cod { get; set; } = 0;
-        [JsonProperty("den")]
-        public string Den { get; set; } = string.Empty;
-        [JsonProperty("usu")]
-        public string Usu { get; set; } = string.Empty;
-        [JsonProperty("cve")]
-        public string Cve { get; set; } = string.Empty;
+        public int Cod { get; set; }
+        [JsonProperty("den"),MaxLength(64)]
+        public string? Den { get; set; } = string.Empty;
+        [JsonProperty("usu"), MaxLength(10)]
+        public string? Usu { get; set; } = string.Empty;
+        [JsonProperty("cve"), MaxLength(10)]
+        public string? Cve { get; set; } = string.Empty;
         [JsonProperty("fch")]
-        public DateTime Fch { get; set; } = DateTime.MinValue;
+        public DateTime? Fch { get; set; } = DateTime.MinValue;
         [JsonProperty("tip")]
-        public int Tip { get; set; } = 0;
+        public int? Tip { get; set; } = 0;
         [JsonProperty("est")]
-        public int Est { get; set; } = 0;
-        [JsonProperty("privilegio")]
-        public string Privilegio { get; set; } = string.Empty;
+        public int? Est { get; set; } = 0;
+        [JsonProperty("privilegio"), MaxLength(256)]
+        public string? Privilegio { get; set; } = string.Empty;
         [JsonProperty("activo")]
-        public int Activo { get; set; } = 0;
+        public bool? Activo { get; set; } = true;
     }
 }
