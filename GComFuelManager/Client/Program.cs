d75b36896063752172e92eed9e1926c7257e530c
Luisromero2811 <19170067@uttcampus.edu.mx>
2023-02-25 13:49:41 -0600

    "Añadi servicio sweetalert2, configure servicios, genere Layout para login"

--- GComFuelManager/Client/Program.cs
+++ GComFuelManager/Client/Program.cs
@@ -1,12 +1,19 @@
 ﻿using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using GComFuelManager.Client;
+using CurrieTechnologies.Razor.SweetAlert2;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddSingleton(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+ConfigureServices(builder.Services);
+
+
 
 await builder.Build().RunAsync();
-
+void ConfigureServices(IServiceCollection services)
+{
+    services.AddSweetAlert2();
+}
