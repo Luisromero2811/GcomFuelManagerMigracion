2f12b770cf58dfcc8eb723b58eac7826faa74145
AngelZapata582 <angelzapata582@gmail.com>
2023-03-01 16:22:53 -0600

    "creacion de dto; modificacion de controladores; visualizacion de datos en el front;"

---
+++ GComFuelManager/Shared/DTOs/CodDenDTO.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GComFuelManager.Shared.DTOs
+{
+    public class CodDenDTO
+    {
+        public int Cod { get; set; } = 0;
+        public string Den { get; set; } = string.Empty;
+    }
+}
