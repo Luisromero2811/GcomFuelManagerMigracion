b94d1707a68077b72c86919bb37ae3334a2c613f
Luisromero2811 <19170067@uttcampus.edu.mx>
2023-03-02 13:58:35 -0600

    "Agregue obtención de datos para unidades(T,CH,V), ordenesDes y usuario"

---
+++ GComFuelManager/Server/Controllers/AsignacionUnidadesController/ChoferController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+using GComFuelManager.Shared.DTOs;
+using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+
+namespace GComFuelManager.Server.Controllers.AsignacionUnidadesController
+{
+    [Route("api/[controller]")]
+    [ApiController]
+	public class ChoferController : ControllerBase 
+	{
+        private readonly ApplicationDbContext context;
+
+        public ChoferController(ApplicationDbContext context)
+		{
+            this.context = context;
+        }
+        [HttpGet("{transportista:int}")]
+        public async Task<ActionResult> Get(int transportista)
+        {
+            try
+            {
+                var transportistas = await context.Chofer
+                    .Where(x => x.Codtransport == transportista && x.Activo == true)
+                    .Select(x => new CodDenDTO { Cod = x.Cod, Den = x.Den!})
+                    .ToListAsync();
+                return Ok(transportistas);
+            }
+            catch(Exception e)
+            {
+                throw e; 
+            }
+        }
+
+	}
+}
+
---
+++ GComFuelManager/Server/Controllers/AsignacionUnidadesController/VehiculoController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+using GComFuelManager.Shared.DTOs;
+using Microsoft.EntityFrameworkCore;
+
+namespace GComFuelManager.Server.Controllers.AsignacionUnidadesController
+{
+    [Route("api/[controller]")]
+    [ApiController]
+	public class VehiculoController : ControllerBase
+	{
+        private readonly ApplicationDbContext context;
+
+        public VehiculoController(ApplicationDbContext context)
+		{
+            this.context = context;
+        }
+        [HttpGet("{chofer:int}")]
+        public async Task<ActionResult> Get(int chofer)
+        {
+            try
+            {
+                var vehiculos = await context.Tonel
+                    .Where(x => Convert.ToInt32(x.Carid) == chofer)
+                    .Select(x => new CodDenDTO { Cod = x.Cod, Den =  $"{x.Tracto} {x.Placatracto} {x.Placa} {x.Capcom!} {x.Capcom2!} {x.Capcom3!} {x.Capcom4!} {x.Codsyn!}" })
+                    .ToListAsync();
+                    return Ok(vehiculos);
+            }
+            catch(Exception e)
+            {
+                throw e;
+            }
+        }
+
+	}
+}
+
+//+ x.Tracto + x.Placatracto
+//+ x.Placa + x.Capcom + " " + x.Capcom2 + " " + x.Capcom3 + " " + x.Capcom4 + " " + x.Codsyn
