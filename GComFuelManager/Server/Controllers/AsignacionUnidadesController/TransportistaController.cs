bab3f870511f6ec47eece062dd9b49a4fbb053a0
Luisromero2811 <19170067@uttcampus.edu.mx>
2023-03-24 09:00:42 -0600

    "Corrección de transportista, diseño y exportaciones de seguimiento ord"

--- GComFuelManager/Server/Controllers/AsignacionUnidadesController/TransportistaController.cs
+++ GComFuelManager/Server/Controllers/AsignacionUnidadesController/TransportistaController.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Http;
 using GComFuelManager.Shared.DTOs;
@@ -21,7 +21,7 @@ namespace GComFuelManager.Server.Controllers.AsignacionUnidadesController
         {
             try
             {
-                var transportistas = await context.Transportista.Where(x => x.activo == true && x.carrId != string.Empty)
+                var transportistas = await context.Transportista.Where(x => x.activo == true && x.gru != null )
                     //.Select(x => new CodDenDTO { Cod = Convert.ToInt32(x.busentid), Den = x.den!})
                     .OrderBy(x => x.den)
                     .ToListAsync();
