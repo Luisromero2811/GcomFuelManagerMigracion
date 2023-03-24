3cc0ee1810854021212e5e0eaf7a9a59bc9f2a9e
Luisromero2811 <19170067@uttcampus.edu.mx>
2023-03-24 09:00:42 -0600

    "Corrección de transportista, diseño y exportaciones de seguimiento ord"

--- GComFuelManager/Server/Controllers/PedidoController/PedidoController.cs
+++ GComFuelManager/Server/Controllers/PedidoController/PedidoController.cs
@@ -256,22 +256,6 @@ namespace GComFuelManager.Server.Controllers
                     .ToListAsync();
                     return Ok(pedidosDate);
                 }
-                else if (fechas.Estado == 4)
-                {
-                    var pedidosDate = await context.Orden
-                    .Where(x => x.Fchcar >= fechas.DateInicio && x.Fchcar <= fechas.DateFin && x.Tonel!.Transportista.activo == true && x.Codprd2 != 10157)
-                    .Include(x => x.Destino)
-                    .ThenInclude(x => x.Cliente)
-                    .Include(x => x.Producto)
-                    .Include(x => x.Tonel)
-                    .ThenInclude(x => x.Transportista)
-                    .Include(x => x.Estado)
-                    .Include(x => x.Chofer)
-                    .OrderBy(x => x.Fchcar)
-                    .Take(10000)
-                    .ToListAsync();
-                    return Ok(pedidosDate);
-                }
                 else
                 {
                     return BadRequest();
@@ -284,7 +268,32 @@ namespace GComFuelManager.Server.Controllers
             }
         }
 
+        [HttpPost("trafico")]
+        public async Task<ActionResult> GetTraffic([FromBody] FechasF fechas)
+        {
+            try
+            {
 
+                var pedidosDate = await context.Orden
+                  .Where(x => x.Fchcar >= fechas.DateInicio && x.Fchcar <= fechas.DateFin && x.Tonel!.Transportista.activo == true && x.Codprd2 != 10157)
+                  .Include(x => x.Destino)
+                  .ThenInclude(x => x.Cliente)
+                  .Include(x => x.Producto)
+                  .Include(x => x.Tonel)
+                  .ThenInclude(x => x.Transportista)
+                  .Include(x => x.Estado)
+                  .Include(x => x.Chofer)
+                  .OrderBy(x => x.Fchcar)
+                  .Take(10000)
+                  .ToListAsync();
+                return Ok(pedidosDate);
+            }
+            catch (Exception e)
+            {
+                return BadRequest(e.Message);
+            }
+           
+        }
         //Method para realizar (agregar) pedido
         [HttpPost]
         public async Task<ActionResult> Post(OrdenEmbarque orden)
