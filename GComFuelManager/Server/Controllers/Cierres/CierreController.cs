1c4687a473a20d9c3977551403b33837d355c2f1
AngelZapata582 <angelzapata582@gmail.com>
2023-03-31 17:52:46 -0600

    "creacion de modulo de cierre de ordenes; avance del modulo de cierre de ordnes;"

---
+++ GComFuelManager/Server/Controllers/Cierres/CierreController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GComFuelManager.Server.Controllers.Cierres
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
+    public class CierreController : ControllerBase
+    {
+        private readonly ApplicationDbContext context;
+
+        public CierreController(ApplicationDbContext context)
+        {
+            this.context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult> Get()
+        {
+            try
+            {
+                return Ok();
+            }
+            catch (Exception e)
+            {
+                return BadRequest(e.Message);
+            }
+        }
+    }
+}
---
+++ GComFuelManager/Shared/Modelos/OrdenCierre.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GComFuelManager.Shared.Modelos
+{
+    public class OrdenCierre
+    {
+        public int Cod { get; set; }
+        public DateTime FchCierre { get; set; } = DateTime.Now;
+        public string Folio { get; set; } = string.Empty;
+        public string Contacto { get; set; } = string.Empty;
+        public string Correo { get; set;} = string.Empty;
+        public int Prd { get; set; }
+        public Producto Producto { get; set; } = null!;
+        public int Cte { get; set; }
+        public Cliente Cliente { get; set; } = null!;
+        public double Precio { get; set; }
+        public double Tempratura { get; set; }
+        public string Vendedor { get; set; } = string.Empty;
+        public int Des { get; set; }
+        public Destino Destino { get; set; } = null!;
+        public int Volumen { get; set; }
+        public string Observaciones { get; set; } = string.Empty;
+    }
+}
