6149177c02cbb2d5147d168375134d1907259e58
AngelZapata582 <angelzapata582@gmail.com>
2023-04-10 17:55:46 -0600

    "agregado modulo para agregar contectos a los clientes;"

--- GComFuelManager/Server/Program.cs
+++ GComFuelManager/Server/Program.cs
@@ -43,6 +43,12 @@ builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
 
 var app = builder.Build();
 
+using(var scope = app.Services.CreateScope())
+{
+    var applicationDbContext = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
+    applicationDbContext.Database.Migrate();
+}
+
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
