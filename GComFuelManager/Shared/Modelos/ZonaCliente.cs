d8618c6da44e89ecedf26854a920d5d513dd61d4
AngelZapata582 <angelzapata582@gmail.com>
2023-05-04 15:28:17 -0600

    "cambio de menu lateral; creacion de pestaña y pagina de precios;"

---
+++ GComFuelManager/Shared/Modelos/ZonaCliente.cs
@@ -0,0 +1,19 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GComFuelManager.Shared.Modelos
+{
+    public class ZonaCliente
+    {
+        [JsonProperty("zonaCod")] public int zonaCod { get; set; }
+        [JsonProperty("cteCod")] public int cteCod { get; set; }
+
+        [NotMapped] public Cliente Cliente { get; set; } = null!;
+        [NotMapped] public Zona Zona { get; set; } = null!;
+    }
+}
