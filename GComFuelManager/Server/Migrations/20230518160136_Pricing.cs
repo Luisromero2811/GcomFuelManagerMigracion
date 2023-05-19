0170c36b57ec6b7f091655bfe732641923698928
Luisromero2811 <19170067@uttcampus.edu.mx>
2023-05-19 09:42:39 -0600

    "Testing y Correcciones de multiples módulos, asignacion de nuevos rol "

---
+++ GComFuelManager/Server/Migrations/20230518160136_Pricing.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace GComFuelManager.Server.Migrations
+{
+    /// <inheritdoc />
+    public partial class Pricing : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(@"insert into AspNetRoles (Id, Name, NormalizedName)
+            values('8a5b6340-5d36-4da3-942c-5e0b5ea2e389','Precios','PRECIOS')");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("delete AspNetRoles where Id = '8a5b6340-5d36-4da3-942c-5e0b5ea2e389'");
+        }
+    }
+}
---
+++ GComFuelManager/Server/Migrations/20230519152739_Obtencionordenes.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace GComFuelManager.Server.Migrations
+{
+    /// <inheritdoc />
+    public partial class Obtencionordenes : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(@"insert into AspNetRoles (Id, Name, NormalizedName)
+               values('b7368b45-3f84-4373-ac17-101389b34f4f','Obtencion de Ordenes','OBTENCION DE ORDENES')");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("delete AspNetRoles where Id = 'b7368b45-3f84-4373-ac17-101389b34f4f'");
+        }
+    }
+}
