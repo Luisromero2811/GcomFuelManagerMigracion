a1f37028a821d2ef591924b74be38574250f359e
AngelZapata582 <angelzapata582@gmail.com>
2023-06-01 09:17:44 -0600

    "agregado modificacion de fechas de venciminto"

---
+++ GComFuelManager/Shared/Modelos/Margen.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GComFuelManager.Shared.Modelos
+{
+    public class Margen
+    {
+        public int Cod { get; set; } = 0;
+        public int CodCte { get; set; } = 0;
+        public double Mar { get; set; } = 0;
+        public string Folio { get; set; } = string.Empty;
+    }
+}
