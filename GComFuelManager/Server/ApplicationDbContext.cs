6a328c28b5a23e629000a6b46fee2f53dcfb3c47
AngelZapata582 <angelzapata582@gmail.com>
2023-07-10 17:27:21 -0600

    "adaptacion de proyecto para el cliente"

--- GComFuelManager/Server/ApplicationDbContext.cs
+++ GComFuelManager/Server/ApplicationDbContext.cs
@@ -244,45 +244,26 @@ namespace GComFuelManager.Server
             //    .HasForeignKey(x => x.Bolguiid);
         }
 
-
-        public DbSet<Actividad> Actividad { get; set; }
-        public DbSet<BitacoraModificacion> BitacoraModificacion { get; set; }
         public DbSet<Chofer> Chofer { get; set; }
-        public DbSet<Ciudad> Ciudad { get; set; }
         public DbSet<Cliente> Cliente { get; set; }
-        public DbSet<Descarga> Descarga { get; set; }
         public DbSet<Destino> Destino { get; set; }
-        public DbSet<Entidad> Entidad { get; set; }
-        public DbSet<FormaPago> FormaPago { get; set; }
         public DbSet<Grupo> Grupo { get; set; }
-        public DbSet<GrupoUsuario> GrupoUsuario { get; set; }
-        public DbSet<Inventario> Inventario { get; set; }
-        public DbSet<Log> Log { get; set; }
-        public DbSet<OrdEmbDet> OrdEmbDet { get; set; }
         public DbSet<Orden> Orden { get; set; }
         public DbSet<OrdenCompra> OrdenCompra { get; set; }
         public DbSet<OrdenEmbarque> OrdenEmbarque { get; set; }
-        public DbSet<Pipa> Pipa { get; set; }
-        public DbSet<Privilegio> Privilegio { get; set; }
-        public DbSet<PrivilegioUsuario> PrivilegioUsuario { get; set; }
         public DbSet<Producto> Producto { get; set; }
         public DbSet<Tad> Tad { get; set; }
         public DbSet<Tonel> Tonel { get; set; }
         public DbSet<Transportista> Transportista { get; set; }
-        public DbSet<TransportistaGrupo> TransportistaGrupo { get; set; }
-        public DbSet<users_descarga> Users_descarga { get; set; }
         public DbSet<Usuario> Usuario { get; set; }
-        public DbSet<Shared.Modelos.Version> Version { get; set; }
         public DbSet<OrdenCierre> OrdenCierre { get; set; }
         public DbSet<Contacto> Contacto { get; set; }
         public DbSet<Precio> Precio { get; set; }
-        public DbSet<PrecioHistorico> PreciosHistorico { get; set; }
         public DbSet<Zona> Zona { get; set; }
         public DbSet<ZonaCliente> ZonaCliente { get; set; }
         public DbSet<OrdenPedido> OrdenPedido { get; set; }
         public DbSet<Accion> Accion { get; set; }
         public DbSet<AccionCorreo> AccionCorreo { get; set; }
         public DbSet<Porcentaje> Porcentaje { get; set; }
-        public DbSet<PrecioProgramado> PrecioProgramado { get; set; }
     }
 }
