df92e2691b44c0336712e1035f165e0d8aadb7ad
Luisromero2811 <19170067@uttcampus.edu.mx>
2023-07-11 12:44:03 -0600

    "Correcciones de controladores"

--- GComFuelManager/Server/Controllers/PedidoController/ClienteController.cs
+++ GComFuelManager/Server/Controllers/PedidoController/ClienteController.cs
@@ -9,7 +9,7 @@ namespace GComFuelManager.Server.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
+    [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme, Roles = "Admin, Administrador Sistema, Direccion, Gerencia, Ejecutivo de Cuenta Comercial, Comprador, Programador, Ejecutivo de Cuenta Operativo, Lectura de Cierre de Orden, Cierre Pedidos, Consulta Precios, Cliente Lectura")]
     public class ClienteController : ControllerBase
     {
         private readonly ApplicationDbContext context;
