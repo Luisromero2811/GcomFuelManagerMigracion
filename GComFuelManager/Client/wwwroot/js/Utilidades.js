b55065e44ddc470203e5d98bc118d62dc7b9af92
Luisromero2811 <19170067@uttcampus.edu.mx>
2023-08-15 11:47:45 -0600

    "Agregado de cierre de sesión por inactividad de 4 minutos fijos"

---
+++ GComFuelManager/Client/wwwroot/js/Utilidades.js
@@ -0,0 +1,14 @@
+﻿function timerInactivo(dotnetHelper) {
+    var timer;
+    document.onmousemove = resetTimer;
+    document.onkeypress = resetTimer;
+
+    function resetTimer() {
+        clearTimeout(timer);
+        timer = setTimeout(logout, 1 * 400000);
+    }
+
+    function logout() {
+        dotnetHelper.invokeMethodAsync("Logoute");
+    }
+}
