56625db87d7acf4776647d773a6b6f6b0a106222
AngelZapata582 <angelzapata582@gmail.com>
2023-08-16 09:16:11 -0600

    "agregado precio con aumento"

---
+++ GComFuelManager/Shared/Modelos/CierrePrecioDespuesFecha.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GComFuelManager.Shared.Modelos
+{
+    public class CierrePrecioDespuesFecha
+    {
+        [Key]
+        public int? Cod { get; set; }
+        public int? CodCie { get; set; }
+        public double? Precio { get; set; }
+        public int? CodCte { get; set; }
+        public short? CodPrd { get; set; }
+    }
+}
