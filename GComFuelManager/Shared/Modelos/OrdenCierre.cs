db5afd76191e43c44e2370d251d152e4169ca83f
Luisromero2811 <19170067@uttcampus.edu.mx>
2023-09-18 12:04:41 -0600

    "Adicion de turno libre, campos de turno y hora en excel de prog equipo"

--- GComFuelManager/Shared/Modelos/OrdenCierre.cs
+++ GComFuelManager/Shared/Modelos/OrdenCierre.cs
@@ -129,6 +129,10 @@ namespace GComFuelManager.Shared.Modelos
 
         [EpplusIgnore, NotMapped]
         public bool IsDifferentVol { get; set; } = false;
+        //Turno cambiado
+        [EpplusIgnore, NotMapped]
+        public bool IsDifferentTurn { get; set; } = false;
+
         [EpplusIgnore, NotMapped]
         public DateTime? FchCar { get; set; } = DateTime.Today;
         [EpplusIgnore, NotMapped]
