fc156b89b6ac374bb40fe5bc763484db774e0e72
AngelZapata582 <angelzapata582@gmail.com>
2023-10-19 00:16:47 -0600

    "optimizacion de tabla de asignacion de orden"

--- GComFuelManager/Shared/Modelos/Chofer.cs
+++ GComFuelManager/Shared/Modelos/Chofer.cs
@@ -7,28 +7,38 @@ using Microsoft.EntityFrameworkCore;
 
 namespace GComFuelManager.Shared.Modelos
 {
-	public class Chofer
-	{
-		[JsonProperty("cod"), Key]
-		public int Cod { get; set; }
+    public class Chofer
+    {
+        [JsonProperty("cod"), Key]
+        public int Cod { get; set; }
 
-		[JsonProperty("den"), MaxLength(128)]
-		public string? Den { get; set; } = string.Empty;
+        [JsonProperty("den"), MaxLength(128)]
+        public string? Den { get; set; } = string.Empty;
 
-		[JsonProperty("codtransport")]
-		public int? Codtransport { get; set; } = 0;
+        [JsonProperty("codtransport")]
+        public int? Codtransport { get; set; } = 0;
 
-		[JsonProperty("dricod"), MaxLength(6)]
-		public string? Dricod { get; set; } = string.Empty;
+        [JsonProperty("dricod"), MaxLength(6)]
+        public string? Dricod { get; set; } = string.Empty;
 
-		[JsonProperty("shortden"), MaxLength(128)]
-		public string? Shortden { get; set; } = string.Empty;
+        [JsonProperty("shortden"), MaxLength(128)]
+        public string? Shortden { get; set; } = string.Empty;
 
-		[JsonProperty("activo")]
-		public bool? Activo { get; set; } = true;
-
-		[NotMapped] public Transportista? Transportista { get; set; } = null!;
-		[NotMapped] public Tonel? Tonel { get; set; } = null!;
+        [JsonProperty("activo")]
+        public bool? Activo { get; set; } = true;
+        [NotMapped]
+        public string FullName
+        {
+            get
+            {
+                if (!string.IsNullOrEmpty(Den) && !string.IsNullOrEmpty(Shortden) && Den.ToLower().Equals(Shortden.ToLower()))
+                    return Shortden;
+                else
+                    return $"{Den} {Shortden}";
+            }
+        }
+        [NotMapped] public Transportista? Transportista { get; set; } = null!;
+        [NotMapped] public Tonel? Tonel { get; set; } = null!;
     }
 }
 
