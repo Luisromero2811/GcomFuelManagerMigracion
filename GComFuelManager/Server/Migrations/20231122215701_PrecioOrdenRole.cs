1798015f20aa649d143b5d6231a6e8620abb21e3
Luisromero2811 <19170067@uttcampus.edu.mx>
2023-11-23 17:23:36 -0600

    "Subi nuevo rol para Armando, Karely, exportación de cierres y vista precios"

---
+++ GComFuelManager/Server/Migrations/20231122215701_PrecioOrdenRole.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace GComFuelManager.Server.Migrations
+{
+    /// <inheritdoc />
+    public partial class PrecioOrdenRole : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(@"insert into AspNetRoles (Id,Name,NormalizedName)
+            values('881d7bcc-2341-433b-9437-609a9b241813','Revision Precio','REVISION PRECIO')");
+
+            migrationBuilder.Sql(@"insert into AspNetRoles (Id,Name,NormalizedName)
+            values('55d29d6a-da57-4166-ba2b-cce0b5a7b0a9','Lectura Asignacion','LECTURA ASIGNACION')");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("delete AspNetRoles where Id = '881d7bcc-2341-433b-9437-609a9b241813'");
+            migrationBuilder.Sql("delete AspNetRoles where Id = '55d29d6a-da57-4166-ba2b-cce0b5a7b0a9'");
+        }
+    }
+}
--- GComFuelManager/Shared/DTOs/FolioDetalleDTO.cs
+++ GComFuelManager/Shared/DTOs/FolioDetalleDTO.cs
@@ -2,6 +2,7 @@
 using OfficeOpenXml.Attributes;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
@@ -11,19 +12,40 @@ namespace GComFuelManager.Shared.DTOs
 {
     public class FolioDetalleDTO
     {
-        public string? Folio { get; set; } = string.Empty;
-        public Cliente? Cliente { get; set; } = null!;
-        public Destino? Destino { get; set; } = null!;
-        public Producto? Producto { get; set; } = null!;
+        [EpplusIgnore]
         public DateTime? FchCierre { get; set; } = null!;
-        public string? Comentarios { get; set; } = null!;
+        [EpplusIgnore]
+        public Cliente? Cliente { get; set; } = null!;
+        [EpplusIgnore]
+        public Destino? Destino { get; set; } = null!;
+        [EpplusIgnore]
+        public Producto? Producto { get; set; } = null!;
         [NotMapped, EpplusIgnore]
         public string? Estado { get; set; } = null!;
         [NotMapped, EpplusIgnore]
         public OrdenEmbarque? ordenEmbarque { get; set; } = null!;
         [NotMapped, EpplusIgnore]
+        public OrdenCierre? ordenCierre { get; set; } = null!;
+        [NotMapped, EpplusIgnore]
         public bool? Activa { get; set; } = true;
         [NotMapped, EpplusIgnore]
         public Grupo? Grupo { get; set; } = null!;
+
+        [DisplayName("Fecha de cierre")]
+        public string FchCie { get { return string.Format(new System.Globalization.CultureInfo("en-US"), "{0:d}", FchCierre); } }
+        [DisplayName("Folio")]
+        public string? Folio { get; set; } = string.Empty;
+        [DisplayName("Grupo")]
+        public string NGrupo { get { return Grupo != null ? Grupo.Den! : string.Empty; } }
+        [DisplayName("Cliente")]
+        public string NCliente { get { return Cliente != null ? Cliente.Den! : string.Empty; } }
+        [DisplayName("Destino")]
+        public string NDestino { get { return Destino != null ? Destino.Den! : string.Empty; } }
+        [DisplayName("Producto")]
+        public string NProducto { get { return Producto != null ? Producto.Den! : string.Empty; } }
+        [DisplayName("Precio")]
+        public double? Precio { get; set; } = 0;
+        [DisplayName("Comentarios")]
+        public string? Comentarios { get; set; } = null!;
     }
 }
