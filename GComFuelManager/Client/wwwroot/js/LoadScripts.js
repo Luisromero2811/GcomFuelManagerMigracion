e1e2815c8de6e4b245a1a26278e037590da18270
AngelZapata582 <angelzapata582@gmail.com>
2023-12-06 17:35:07 -0600

    "agregado solucion de redimensionamiento de tabla de asignacion de orden"

---
+++ GComFuelManager/Client/wwwroot/js/LoadScripts.js
@@ -0,0 +1,7 @@
+﻿console.log("cargado");
+window.importarScript = function (rutaScript) {
+    console.log(rutaScript);
+    var script = document.createElement("script");
+    script.src = rutaScript + '?t=' + new Date().getTime(); //agregar un parametro unico para evitar la cache
+    document.head.appendChild(script);
+}
--- GComFuelManager/Client/wwwroot/js/resizableTable.js
+++ GComFuelManager/Client/wwwroot/js/resizableTable.js
@@ -1,7 +1,26 @@
-﻿window.makeTableResizable = () => {
-    $("#miTabla th").resizable({
-        handles: "e", // Permitir redimensionar desde el borde derecho
-        minWidth: 20, // Ancho mínimo permitido
-        maxWidth: 400 // Ancho máximo permitido
+﻿$(function () {
+    var pressed = false;
+    var start = undefined;
+    var startX, startWidth;
+
+    $("table th").mousedown(function (e) {
+        start = $(this);
+        pressed = true;
+        startX = e.pageX;
+        startWidth = $(this).width();
+        $(start).addClass("resizing");
+    })
+
+    $(document).mousemove(function (e) {
+        if (pressed) {
+            $(start).width(startWidth + (e.pageX - startX));
+        }
     });
-};
+
+    $(document).mouseup(function () {
+        if (pressed) {
+            $(start).removeClass("resizing");
+            pressed = false;
+        }
+    });
+});
