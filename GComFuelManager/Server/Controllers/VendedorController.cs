3fd91313fa7f289b53371504e70f0ee7aaed46bc
AngelZapata582 <angelzapata582@gmail.com>
2024-02-23 13:13:08 -0600

    Correccion
    - correccion de roles

--- GComFuelManager/Server/Controllers/VendedorController.cs
+++ GComFuelManager/Server/Controllers/VendedorController.cs
@@ -15,7 +15,7 @@ namespace GComFuelManager.Server.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme, Roles = "Admin, Administrador, Reporte De Venta")]
+    [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme, Roles = "Admin, Administrador, Reportes De Venta")]
     public class VendedorController : ControllerBase
     {
         private readonly ApplicationDbContext context;
