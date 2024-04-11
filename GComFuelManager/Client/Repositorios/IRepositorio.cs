a613d25bead772f323556a03de82ec201d5d4335
AngelZapata582 <angelzapata582@gmail.com>
2024-04-11 12:33:12 -0600

    avance

--- GComFuelManager/Client/Repositorios/IRepositorio.cs
+++ GComFuelManager/Client/Repositorios/IRepositorio.cs
@@ -5,7 +5,9 @@ namespace GComFuelManager.Client.Repositorios
 	public interface IRepositorio
 	{
         Task<HttpResponseWrapper<object>> Post<T>(string url, T enviar);
+        Task<HttpResponseWrapper<object>> PostFile(string url, MultipartFormDataContent enviar);
         Task<HttpResponseWrapper<TResponse>> Post<T, TResponse>(string url, T enviar);
+        Task<HttpResponseWrapper<TResponse>> PostFile<TResponse>(string url, MultipartFormDataContent enviar);
         Task<HttpResponseWrapper<T>> Get<T>(string url);
         Task<HttpResponseWrapper<object>> Put<T>(string url, T enviar);
         Task<HttpResponseWrapper<object>> Delete(string url);
