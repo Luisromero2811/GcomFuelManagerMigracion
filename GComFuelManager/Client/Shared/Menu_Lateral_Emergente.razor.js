b7724db72cf8140dffa42e1fc85e69d110bad650
AngelZapata582 <angelzapata582@gmail.com>
2024-06-01 16:39:55 -0600

    Avance de alerta

--- GComFuelManager/Client/Shared/Menu_Lateral_Emergente.razor.js
+++ GComFuelManager/Client/Shared/Menu_Lateral_Emergente.razor.js
@@ -5,9 +5,7 @@
 window.addEventListener('click', function (e) {
     if (document.getElementById('mySidepanel').contains(e.target) || document.getElementById('buttonSidepanel').contains(e.target)) {
         // this.alert("clic in")
-        console.log("clic in")
     } else {
-        console.log("clic out")
         document.getElementById("mySidepanel").style.width = "0px";
     }
 })
