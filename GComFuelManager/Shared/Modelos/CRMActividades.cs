964165b248f9238c6a3ca499f4c3c8c07a061e78
AngelZapata582 <angelzapata582@gmail.com>
2024-08-16 12:55:21 -0600

    Agregado
    - agregado modelos de CRM
    - agregado fluentvalidation y automapper a proyecto server

---
+++ GComFuelManager/Shared/Modelos/CRMActividades.cs
@@ -0,0 +1,20 @@
+﻿namespace GComFuelManager.Shared.Modelos
+{
+    public class CRMActividad
+    {
+        public int Id { get; set; }
+        public int Asunto { get; set; }
+        public DateTime Fecha_Creacion { get; set; } = DateTime.Now;
+        public DateTime Fecha_Mod { get; set; }
+        public DateTime Fecha_Inicio { get; set; }
+        public DateTime Fecha_Fin { get; set; }
+        public DateTime Fecha_Ven { get; set; }
+        public int Prioridad { get; set; }
+        public int Asignado { get; set; }
+        public string Descripcion { get; set; } = string.Empty;
+        public int Estatus { get; set; }
+        public int Contacto_Rel { get; set; }
+        public int Recordatorio { get; set; }
+        public bool Activo { get; set; } = true;
+    }
+}
