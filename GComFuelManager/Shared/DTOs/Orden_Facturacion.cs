30a040ef3e5e04e889f7f7a630f58f19082ace9f
Luisromero2811 <19170067@uttcampus.edu.mx>
2024-11-12 15:31:54 -0600

    "Complemento de archivo de facturación"

--- GComFuelManager/Shared/DTOs/Orden_Facturacion.cs
+++ GComFuelManager/Shared/DTOs/Orden_Facturacion.cs
@@ -14,28 +14,32 @@ namespace GComFuelManager.Shared.DTOs
     {
         //public long? Id_Orden { get; set; }
         public string? Terminal { get; set; } = string.Empty;
+        public string? Destino { get; set; } = string.Empty;
+        [DisplayName("Tipo de venta")]
+        public string? Tipo_Venta { get; set; } = string.Empty;
+        public string? Producto { get; set; } = string.Empty;
+        [DisplayName("Volumen cargado")]
+        public double? Volumen_Cargado { get; set; }
+        [DisplayName("Importe de Compra")]
+        public string? Importe_Compra { get; set; }
+        [DisplayName("Precio Compra s/impuesto")]
+        public double? Precio_CompraSImpuesto { get; set; }
+        [DisplayName("BOL/Emarque")]
+        public string? BOL { get; set; } = string.Empty;
+        public string? Factura_Proveeedor { get; set; } = string.Empty;
         [EpplusIgnore]
         public DateTime? Fecha_Carga { get; set; }
         [DisplayName("Fecha de carga")]
         public string? Fecha_Carga_Formato { get => Fecha_Carga?.ToString("d"); }
-        [DisplayName("BOL/Emarque")]
-        public string? BOL { get; set; } = string.Empty;
-        public double? Precio { get; set; }
-        [DisplayName("Tipo de venta")]
-        public string? Tipo_Venta { get; set; } = string.Empty;
-        public string? Cliente { get; set; } = string.Empty;
-        public string? Destino { get; set; } = string.Empty;
-        public string? Producto { get; set; } = string.Empty;
-        [DisplayName("Volumen cargado")]
-        public double? Volumen_Cargado { get; set; }
         public string? Tranportista { get; set; } = string.Empty;
-        public string? Unidad { get; set; } = string.Empty;
         public string? Chofer { get; set; } = string.Empty;
-        public string? Sellos { get; set; } = string.Empty;
-        public string? Pedimento { get; set; } = string.Empty;
+        public string? Unidad { get; set; } = string.Empty;
+        public string? Cliente { get; set; } = string.Empty;
+        public double? Precio { get; set; }
         [DisplayName("Numero de orden")]
         public string? No_Orden { get; set; } = string.Empty;
-        public string? Factura_Proveeedor { get; set; } = string.Empty;
+        public string? Sellos { get; set; } = string.Empty;
+        public string? Pedimento { get; set; } = string.Empty;
         [EpplusIgnore]
         public List<Archivo> Archivos { get; set; } = new();
         //public string? Referencia { get; set; } = string.Empty;
