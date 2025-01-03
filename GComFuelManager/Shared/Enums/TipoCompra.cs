1a7ae9338353d743ccfef29524976618a06c734e
Luisromero2811 <19170067@uttcampus.edu.mx>
2025-01-03 16:15:16 -0600

    "Nuevo elementos para modelo de compra"

--- GComFuelManager/Shared/Enums/TipoCompra.cs
+++ GComFuelManager/Shared/Enums/TipoCompra.cs
@@ -1,10 +1,15 @@
-﻿namespace GComFuelManager.Shared.Enums
+﻿using System.ComponentModel;
+
+namespace GComFuelManager.Shared.Enums
 {
     //Pruebas para producción
     public enum TipoCompra
     {
+        [Description("Rack")]
         Rack,
+        [Description("Delivery")]
         Delivery,
+        [Description("N/A")]
         NA
     }
 }
